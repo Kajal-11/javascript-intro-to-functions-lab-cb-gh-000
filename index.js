9d64ec1a0d71184272adde15c451c54f8e4693c3
Kajal Goel <kajal11122000@gmail.com>
2019-06-14 12:06:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function shout(aLine){
     return aLine.toUpperCase()
 }
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  string.toUpperCase()
+  console.log(string)
+}
